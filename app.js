d9211e196297b2e3286627dc192f326f763d524e
Timothy Miskelley <miskelleytimothy@gmail.com>
2021-11-08 21:56:28 -0500

    Added basic functionality

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function multiply(f, s) {
 }
 
 function divide(f, s) {
-    return f / s;
+    return (f / s).toFixed(3);
 }
 
 function operate(op, num1, num2) {
@@ -26,3 +26,68 @@ function operate(op, num1, num2) {
             return divide(num1, num2);
     }
 }
+
+function updateDisplay(currentNumber) {
+    if(first) {
+        display.textContent = currentNumber;
+    } else {
+        first = true;
+        display.textContent = currentNumber;
+    }
+}
+
+function clearDisplay() {
+    first = false;
+    display.textContent = 0;
+    currentNumber = 0;
+}
+
+let first = false;
+let currentNumber = 0;
+let operator = 0;
+let num1 = 0;
+let num2 = 0;
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.id == 'clear') {
+            clearDisplay();
+            currentNumber = 0;
+        } else if(button.id == 'equals') {
+            num2 = currentNumber;
+            let ans = parseFloat(operate(operator, num1, num2));
+            updateDisplay(ans);
+            currentNumber = ans;
+        } else if(button.id == 'negative') {
+            currentNumber *= -1;
+            updateDisplay(currentNumber);
+        } else if(button.id == 'add') {
+            operator = 0;
+            num1 = currentNumber;
+            currentNumber = 0;
+        } else if(button.id == 'minus') {
+            operator = 1;
+            num1 = currentNumber;
+            currentNumber = 0;
+        } else if(button.id == 'multiply') {
+            operator = 2;
+            num1 = currentNumber;
+            currentNumber = 0;
+        } else if(button.id == 'divide') {
+            operator = 3;
+            num1 = currentNumber;
+            currentNumber = 0;
+        } else if(button.id == 'percent') {
+            currentNumber /= 10;
+            updateDisplay(currentNumber);
+        } else if(first) {
+            currentNumber = (currentNumber * 10) + parseInt(button.id);
+            updateDisplay(currentNumber);
+        } else {
+            currentNumber += parseInt(button.id);
+            updateDisplay(currentNumber);
+        }
+    });
+});
